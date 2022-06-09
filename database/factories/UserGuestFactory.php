2ba2429efcfaa00ff8059ad10be61be501b470fd
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-09 17:05:49 +0200

    create model all

---
+++ database/factories/UserGuestFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UserGuest;
+use Faker\Generator as Faker;
+
+$factory->define(UserGuest::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
