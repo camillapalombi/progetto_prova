bbf647aa76ef37b141944f67abae2dfaedd6b6ee
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-06-09 17:21:30 +0200

    create-userGuest-seeder

---
+++ database/seeds/UserGuestSeeder.php
@@ -0,0 +1,24 @@
+<?php
+      use Illuminate\Database\Seeder;
+      use Faker\Generator as Faker;
+      use App\UserGuest;
+      class UserGuestSeeder extends Seeder
+      {
+         public function run(Faker $faker)
+         {
+            $faker ->addProvider(new \Faker\Provider\it_IT\Person($faker));
+            for ($i=0; $i <50 ; $i++) {
+                $title = $faker->words(rand(2, 5), true);
+                UserGuest::create([
+                    'name'      =>  $faker->first_name(),
+                    'surname'   =>  $faker->last_name(),
+                    'mail'      =>  $faker->email(),
+                    'username'  =>  $faker->userName(),
+                    'password'  =>  $faker->password(),
+                  ]);
+                //   $user = new UserGuestSeeder();
+                  $user->save();
+            }
+         }
+      }
+
