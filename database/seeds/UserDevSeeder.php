dbbdaf8bde701b27ba9c6f41c7a88bd2a426c790
ErikaSarleti <erikasarleti00@gmail.com>
2022-06-09 17:20:18 +0200

    seeder

--- database/seeds/UserDevSeeder.php
+++ database/seeds/UserDevSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserDevSeeder extends Seeder
 {
@@ -9,8 +12,19 @@ class UserDevSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        User::create([
+            'name' => $faker->name(),
+            'surname'=> $faker->surname(),
+            'email'=>$faker->email(),
+            'password'=> Hash::make('ciaociao'),
+            'cv'=> $faker->imageUrl(360, 360, 'animals', true, 'cats'),
+            'località'=> $faker->city(),
+            'github'=> $faker->name(),
+            'presentazione'=> $faker->paragraph(),
+            'photo'=> $faker->imageUrl(360, 360, 'animals', true, 'cats'),
+
+        ]);
     }
 }
